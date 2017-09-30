deed2acaad02ab0497539886dca0eb51fdaa256e
Sachiv Paruchuri <sachivpvlns@gmail.com>
2017-10-01 00:56:31 +0530

    Connecting chatbot api

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,9 @@
-var bot_avatar_img_url = 'dist/images/bot-avatar.png';
-var bot_name = 'Cyborg';
-var guest_name = 'Guest';
+var bot_avatar_img_url = 'dist/images/bot-avatar.png',
+    chatBotName = 'Cyborg',
+    guest_name = 'Guest',
+    apiURL = 'https://www.personalityforge.com/api/chat/',
+    apiKey = '__YOUR__KEY__', // use your own key
+    chatBotID = '63906';
 
 var myApp = new Framework7();
 
@@ -21,38 +24,68 @@ var myMessagebar = myApp.messagebar('.messagebar', {
 
 // Handle message
 $$('.messagebar .link').on('click', function () {
-    // Message text
-    var messageText = myMessagebar.value().trim();
-    // Exit if empy message
-    if (messageText.length === 0) return;
+    addMessage('sent');
+});
 
-    // Empty messagebar
-    myMessagebar.clear()
+function addMessage(messageType, messageText, chatBotName) {
+    if (messageType == 'sent' || messageType == 'received') {
+        // Avatar and name for received message
+        var avatar, name;
+        if (messageType === 'received') {
+            avatar = bot_avatar_img_url;
+            if (chatBotName) {
+                name = chatBotName;
+            } else {
+                name = chatBotName;
+            }
+        } else {
+            name = guest_name;
+            // Message text
+            if (!messageText) {
+                messageText = myMessagebar.value().trim();
+            }
+            // Exit if empy message
+            if (messageText.length === 0) return;
 
-    // Random message type
-    var messageType = (['sent', 'received'])[Math.round(Math.random())];
+            // Empty messagebar
+            myMessagebar.clear();
+        }
+        // Add message
+        myMessages.addMessage({
+            // Message text
+            text: messageText,
+            // Random message type
+            type: messageType,
+            // Avatar and name:
+            avatar: avatar,
+            name: name,
+            // Day
+            day: !conversationStarted ? 'Today' : false,
+            time: !conversationStarted ? (new Date()).getHours() + ':' + (new Date()).getMinutes() : false
+        })
+        // Update conversation flag
+        conversationStarted = true;
 
-    // Avatar and name for received message
-    var avatar, name;
-    name = guest_name;
-    if (messageType === 'received') {
-        avatar = bot_avatar_img_url;
-        name = guest_name;
+        if (messageType === 'sent') {
+            askChatbot(messageText);
+        }
     }
-    // Add message
-    myMessages.addMessage({
-        // Message text
-        text: messageText,
-        // Random message type
-        type: messageType,
-        // Avatar and name:
-        avatar: avatar,
-        name: name,
-        // Day
-        day: !conversationStarted ? 'Today' : false,
-        time: !conversationStarted ? (new Date()).getHours() + ':' + (new Date()).getMinutes() : false
-    })
+}
 
-    // Update conversation flag
-    conversationStarted = true;
-});                
+function askChatbot(messageText) {
+    //do get request
+    $$.get(apiURL, {
+        apiKey: apiKey,
+        chatBotID: chatBotID,
+        externalID: guest_name,
+        message: messageText
+    }, function (data) {
+        data = JSON.parse(data);
+        if (data.success) {
+            chatBotName = data.message.chatBotName;
+            addMessage('received', data.message.message, chatBotName);
+        } else {
+            addMessage('received', data.errorType + " :: " + data.errorMessage, chatBotName);
+        }
+    });
+}
